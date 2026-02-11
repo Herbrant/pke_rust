ed9e82c9dc09381c9bb78c29b70f8b14ed3d812b
Herbrant <cdavide98carnemolla@gmail.com>
2026-02-11 22:42:51 +0100

    removed unused code

--- benches/el_gamal.rs
+++ benches/el_gamal.rs
@@ -2,16 +2,14 @@ use divan::black_box;
 use divan::Bencher;
 use pke_rust::el_gamal::algorithms::ElGamal;
 use pke_rust::traits::public_enc::PublicEnc;
-use pke_rust::utils::rand::rug_randseed_os_rng;
 use rug::rand::RandState;
 
 const SECURY_LEVEL: u64 = 80;
 const VALUES: &[u64] = &[1, 5, 10, 20, 30, 40];
 
-#[divan::bench(max_time = 20)]
+#[divan::bench()]
 fn el_gamal_keygen(bencher: Bencher) {
     let mut rng = RandState::new();
-    rug_randseed_os_rng(80, &mut rng).unwrap();
 
     bencher.bench_local(|| {
         let _ = ElGamal::keygen(black_box(SECURY_LEVEL), black_box(&mut rng)).unwrap();
@@ -21,7 +19,6 @@ fn el_gamal_keygen(bencher: Bencher) {
 #[divan::bench(args=VALUES)]
 fn el_gamal_encrypt(bencher: Bencher, m: u64) {
     let mut rng = RandState::new();
-    rug_randseed_os_rng(80, &mut rng).unwrap();
     let (_, pk) = ElGamal::keygen(SECURY_LEVEL, &mut rng).unwrap();
 
     let m = m.to_le_bytes();
